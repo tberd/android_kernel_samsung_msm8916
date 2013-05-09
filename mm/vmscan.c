1f3d4a8d9faf4748602a05e8692340e9b295ee4a
Minchan Kim <minchan@kernel.org>
2013-05-09 16:21:26 +0900

    mm: Remove shrink_page
    By previous patch, shrink_page_list can handle pages from
    multiple zone so let's remove shrink_page.
    
    Change-Id: I3526377aa6ee6142b8f3ec63396e7ada1e442505
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Patch-mainline: linux-mm @ 22 Apr 2013 17:45:03
    [vinmenon@codeaurora.org: trivial merge conflict fixes]
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1244,8 +1244,7 @@ unsigned long reclaim_clean_pages_from_list(struct zone *zone,
 }
 
 #ifdef CONFIG_PROCESS_RECLAIM
-unsigned long reclaim_pages_from_list(struct list_head *page_list,
-					struct vm_area_struct *vma)
+unsigned long reclaim_pages_from_list(struct list_head *page_list)
 {
 	struct scan_control sc = {
 		.gfp_mask = GFP_KERNEL,
