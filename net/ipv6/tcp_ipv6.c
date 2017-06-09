6b352eb66a61c92db07d420663ec54f279517cf6
Chenbo Feng <fengc@google.com>
2017-06-09 12:17:37 -0700

    FROMLIST: bpf: Remove duplicate tcp_filter hook in ipv6
    There are two tcp_filter hooks in tcp_ipv6 ingress path currently.
    One is at tcp_v6_rcv and another is in tcp_v6_do_rcv. It seems the
    tcp_filter() call inside tcp_v6_do_rcv is redundent and some packet
    will be filtered twice in this situation. This will cause trouble
    when using eBPF filters to account traffic data.
    
    Signed-off-by: Chenbo Feng <fengc@google.com>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    (url: http://patchwork.ozlabs.org/patch/774126/)
    Bug: 30950746
    Change-Id: Id4fe8cd5b7bac11a4d4141e203dd4b9fa59f3d6c
    
    Change-Id: Iaea5e389d815d837c041c83afce4adcbde489acb

--- net/ipv6/tcp_ipv6.c
+++ net/ipv6/tcp_ipv6.c
@@ -1353,8 +1353,10 @@ static int tcp_v6_do_rcv(struct sock *sk, struct sk_buff *skb)
 		goto discard;
 #endif
 
-	if (tcp_filter(sk, skb))
-		goto discard;
+#ifdef CONFIG_LGP_DATA_TCPIP_MPTCP
+	if (is_meta_sk(sk))
+		return mptcp_v6_do_rcv(sk, skb);
+#endif
 
 	/*
 	 *	socket locking is here for SMP purposes as backlog rcv
