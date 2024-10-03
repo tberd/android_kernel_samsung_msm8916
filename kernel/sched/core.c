0774b9996aeaa9c1c0f46b4f4324037fa61607fa
mukul2259 <dhirmukul3@gmail.com>
2024-10-04 01:38:09 +0200

    [TMP] kernelwide: Drop vendor cruft for now
     * Causing build errors, revisit later
    
     * /home/undead/los/kernel/oneplus/msm8994/kernel/sched/core.c: In function 'cpu_cfs_quota_per_task_read_s64':
    /home/undead/los/kernel/oneplus/msm8994/kernel/sched/core.c:10188:2: error: implicit declaration of function 'cgroup_tg' [-Werror=implicit-function-declaration]
      return tg_get_cfs_quota_per_task(cgroup_tg(cgrp));
      ^
    /home/undead/los/kernel/oneplus/msm8994/kernel/sched/core.c:10188:35: warning: passing argument 1 of 'tg_get_cfs_quota_per_task' makes pointer from integer without a cast
      return tg_get_cfs_quota_per_task(cgroup_tg(cgrp));
                                       ^
    /home/undead/los/kernel/oneplus/msm8994/kernel/sched/core

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -10191,19 +10191,6 @@ long tg_get_cfs_period(struct task_group *tg)
 	return cfs_period_us;
 }
 
-#ifdef VENDOR_EDIT
-static s64 cpu_cfs_quota_per_task_read_s64(struct cgroup *cgrp, struct cftype *cft)
-{
-	return tg_get_cfs_quota_per_task(cgroup_tg(cgrp));
-}
-
-static int cpu_cfs_quota_per_task_write_s64(struct cgroup *cgrp, struct cftype *cftype,
-				s64 cfs_quota_us)
-{
-	return tg_set_cfs_quota_per_task(cgroup_tg(cgrp), cfs_quota_us);
-}
-#endif
-
 static s64 cpu_cfs_quota_read_s64(struct cgroup_subsys_state *css,
 				  struct cftype *cft)
 {
